04cb4a44de80491a55fb6d14e3fb8e2e65924617
Austin Barnes <abarnes@scrippsnetworks.com>
2016-08-07 22:08:41 -0400

    xcode8 fixes

--- Source/SwiftyJSON.swift
+++ Source/SwiftyJSON.swift
@@ -631,7 +631,7 @@ extension JSON {
 }
 
 // MARK: - Bool
-
+/*
 extension JSON: Swift.Boolean {
 
     //Optional bool
@@ -668,6 +668,7 @@ extension JSON: Swift.Boolean {
         }
     }
 }
+ */
 
 // MARK: - String
 
@@ -736,8 +737,8 @@ extension JSON {
             switch self.type {
             case .string:
                 let decimal = NSDecimalNumber(string: self.object as? String)
-                if decimal == NSDecimalNumber.notANumber() {  // indicates parse error
-                    return NSDecimalNumber.zero()
+                if decimal == NSDecimalNumber.notANumber {  // indicates parse error
+                    return NSDecimalNumber.zero
                 }
                 return decimal
             case .number, .bool:
